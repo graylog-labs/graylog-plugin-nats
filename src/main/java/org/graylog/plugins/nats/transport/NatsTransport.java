598a7286461ecbbcb01912e4992c2676d4dabe46
Jochen Schalanda <jochen@graylog.com>
2016-10-27 12:27:07 +0200

    Upgrade to license-maven-plugin 3.0

--- src/main/java/org/graylog/plugins/nats/transport/NatsTransport.java
+++ src/main/java/org/graylog/plugins/nats/transport/NatsTransport.java
@@ -1,18 +1,19 @@
 /**
- * This file is part of Graylog NATS Plugin.
+ * Graylog NATS Plugin - NATS plugin for Graylog
+ * Copyright © 2016 Graylog, Inc. (hello@graylog.com)
  *
- * Graylog NATS Plugin is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Graylog NATS Plugin is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Graylog NATS Plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.graylog.plugins.nats.transport;
 
